d034a9d7e45536f63c0cae4703be4b38bab75ca9
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-06-19 22:44:27 +0530

    Fix eslint errors in route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "todos#index"
-  resources :todos, only: [:index, :create, :destroy, :update]
+  resources :todos, only: [ :index, :create, :destroy, :update ]
 end
