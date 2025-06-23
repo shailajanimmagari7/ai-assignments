1bd3079e2b762c80ec40ce047ff372c6e617f846
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-06-23 10:14:23 +0530

    Add uniqueness validation  to not allow the duplicate data

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,3 @@
 class Todo < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :title, presence: true, uniqueness: true, length: { minimum: 3, maximum: 100 }
 end
